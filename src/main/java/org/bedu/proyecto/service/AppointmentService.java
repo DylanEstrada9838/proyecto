10dd14f9be1e2f41004530aafbc9ba15d96b5e0f
MisaOro24 <ma414395@uaeh.edu.mx>
2024-02-07 15:51:05 -0600

    "Adding repository test"

---
+++ src/main/java/org/bedu/proyecto/service/AppointmentService.java
@@ -0,0 +1,101 @@
+package org.bedu.proyecto.service;
+
+import org.bedu.proyecto.dto.appointment.AppointmentDTO;
+import org.bedu.proyecto.dto.appointment.ChangeStatusAppointmentDTO;
+import org.bedu.proyecto.dto.appointment.CreateAppointmentDTO;
+import org.bedu.proyecto.exception.appointment.AppointmentAlreadyCompleted;
+import org.bedu.proyecto.exception.appointment.AppointmentAlreadyExist;
+import org.bedu.proyecto.exception.appointment.AppointmentCreationNotAllowed;
+import org.bedu.proyecto.exception.appointment.AppointmentNotFound;
+import org.bedu.proyecto.exception.appointment.CannotChangeStatus;
+import org.bedu.proyecto.exception.quote.QuoteNotFound;
+import org.bedu.proyecto.mapper.AppointmentMapper;
+import org.bedu.proyecto.model.Appointment;
+import org.bedu.proyecto.model.Quote;
+import org.bedu.proyecto.model.ServiceRequest;
+import org.bedu.proyecto.model_enums.StatusAppointment;
+import org.bedu.proyecto.model_enums.StatusQuote;
+import org.bedu.proyecto.model_enums.StatusRequest;
+import org.bedu.proyecto.repository.AppointmentRepositoryTest;
+import org.bedu.proyecto.repository.QuoteRepository;
+import org.bedu.proyecto.repository.ServiceRequestRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AppointmentService {
+    @Autowired
+    private AppointmentRepositoryTest repository;
+    @Autowired
+    private AppointmentMapper mapper;
+    @Autowired
+    private QuoteRepository quoteRepository;
+    @Autowired
+    private ServiceRequestRepository serviceRequestRepository;
+
+    public AppointmentDTO save(long quoteId,CreateAppointmentDTO data) throws QuoteNotFound, AppointmentAlreadyExist, AppointmentCreationNotAllowed{
+        Quote quote = Validation.quoteExists(quoteRepository, quoteId);
+
+        //Validation Quote have ACCEPTED status
+        if(quote.getStatus() != StatusQuote.ACCEPTED){
+            throw new AppointmentCreationNotAllowed(quoteId);
+        }
+
+        //Validation Appointment is not already created for Quote
+        if(quote.getAppointment()!=null){
+            throw new AppointmentAlreadyExist(quoteId);
+        }
+
+        Appointment entity = mapper.toModel(data);
+        entity.setQuote(quote);
+        repository.save(entity);
+
+        //Change Quote status to scheduled
+        quote.setStatus(StatusQuote.SCHEDULED);
+        quoteRepository.save(quote);
+
+        //Change ServiceRequest status to scheduled
+        ServiceRequest serviceRequest = quote.getQuoteRequest().getServiceRequest();
+        serviceRequest.setStatus(StatusRequest.SCHEDULED);
+        serviceRequestRepository.save(serviceRequest);
+
+
+        return mapper.toDTO(entity);
+    }
+
+    public AppointmentDTO findByQuote(long quoteId) throws QuoteNotFound, AppointmentNotFound{
+        Quote quote = Validation.quoteExists(quoteRepository, quoteId);
+        if(quote.getAppointment()==null){
+            throw new AppointmentNotFound(quoteId);
+        }
+        return mapper.toDTO(quote.getAppointment());
+
+    }
+
+    public void update (long appointmentId, ChangeStatusAppointmentDTO data) throws AppointmentNotFound, AppointmentAlreadyCompleted, CannotChangeStatus{
+        Appointment appointment=Validation.appointmentExists(repository, appointmentId);
+
+        //Validation, Supplier can only change status to COMPLETED
+        if(data.getStatus()!= StatusAppointment.COMPLETED){
+            throw new CannotChangeStatus(appointmentId);
+        }
+
+        //Validation appointment is not already with COMPLETED Status
+        if(appointment.getStatus()==StatusAppointment.COMPLETED){
+            throw new AppointmentAlreadyCompleted(appointmentId);
+        }
+
+        mapper.update(appointment, data);
+        repository.save(appointment);
+
+        //Change Quote status to COMPLETED
+        Quote quote = appointment.getQuote();
+        quote.setStatus(StatusQuote.COMPLETED);
+        quoteRepository.save(quote);
+
+        //Change ServiceRequest to COMPLETED
+        ServiceRequest serviceRequest = appointment.getQuote().getQuoteRequest().getServiceRequest();
+        serviceRequest.setStatus(StatusRequest.COMPLETED);
+        serviceRequestRepository.save(serviceRequest);
+    }
+}
---
+++ src/main/java/org/bedu/proyecto/service/Validation.java
@@ -0,0 +1,121 @@
+package org.bedu.proyecto.service;
+
+import org.bedu.proyecto.exception.address.AddressNotFound;
+import org.bedu.proyecto.exception.appointment.AppointmentNotFound;
+import org.bedu.proyecto.exception.client.ClientNotFoundException;
+import org.bedu.proyecto.exception.quote.QuoteNotFound;
+import org.bedu.proyecto.exception.quote_request.QuoteRequestNotFound;
+import org.bedu.proyecto.exception.request.ServiceRequestNotFound;
+import org.bedu.proyecto.exception.service.ServiceNotFoundException;
+import org.bedu.proyecto.exception.supplier.ServiceNotAssignedException;
+import org.bedu.proyecto.exception.supplier.SupplierNotFoundException;
+import org.bedu.proyecto.exception.user.UserNotFoundException;
+import org.bedu.proyecto.keys.SupplierServiceKey;
+import org.bedu.proyecto.model.Address;
+import org.bedu.proyecto.model.AppService;
+import org.bedu.proyecto.model.Appointment;
+import org.bedu.proyecto.model.Client;
+import org.bedu.proyecto.model.Quote;
+import org.bedu.proyecto.model.QuoteRequest;
+import org.bedu.proyecto.model.ServiceRequest;
+import org.bedu.proyecto.model.Supplier;
+import org.bedu.proyecto.model.SupplierServiceJoin;
+import org.bedu.proyecto.model.User;
+import org.bedu.proyecto.repository.AddressRepository;
+import org.bedu.proyecto.repository.AppointmentRepositoryTest;
+import org.bedu.proyecto.repository.ClientRepository;
+import org.bedu.proyecto.repository.QuoteRepository;
+import org.bedu.proyecto.repository.QuoteRequestRepository;
+import org.bedu.proyecto.repository.ServiceRepository;
+import org.bedu.proyecto.repository.ServiceRequestRepository;
+import org.bedu.proyecto.repository.SupplierRepository;
+import org.bedu.proyecto.repository.SupplierServiceJoinRepository;
+import org.bedu.proyecto.repository.UserRepository;
+
+public class Validation {
+    // Supplier
+    public static Supplier supplierExists(SupplierRepository repository, long supplierId)
+            throws SupplierNotFoundException {
+        return repository.findById(supplierId).orElseThrow(() -> new SupplierNotFoundException(supplierId));
+    }
+
+    public static void verifySupplierExists(SupplierRepository repository, long supplierId)
+            throws SupplierNotFoundException {
+        if (!repository.existsById(supplierId)) {
+            throw new SupplierNotFoundException(supplierId);
+        }
+    }
+
+    // User
+    public static User userExists(UserRepository repository, long userId) throws UserNotFoundException {
+        return repository.findById(userId).orElseThrow(() -> new UserNotFoundException(userId));
+    }
+
+    // Service
+    public static AppService serviceExists(ServiceRepository repository, long serviceId)
+            throws ServiceNotFoundException {
+        return repository.findById(serviceId).orElseThrow(() -> new ServiceNotFoundException(serviceId));
+    }
+
+    public static void verifyServiceExists(ServiceRepository repository, long serviceId)
+            throws ServiceNotFoundException {
+        if (!repository.existsById(serviceId)) {
+            throw new ServiceNotFoundException(serviceId);
+        }
+    }
+
+    // Client
+    public static Client clientExists(ClientRepository repository, long clientId) throws ClientNotFoundException {
+        return repository.findById(clientId).orElseThrow(() -> new ClientNotFoundException(clientId));
+    }
+
+    public static void verifyClientExists(ClientRepository repository, long clientId) throws ClientNotFoundException {
+        if (!repository.existsById(clientId)) {
+            throw new ClientNotFoundException(clientId);
+        }
+    }
+
+    // ServiceRequest
+    public static ServiceRequest serviceRequestExists(ServiceRequestRepository repository, long serviceRequestId)
+            throws ServiceRequestNotFound {
+        return repository.findById(serviceRequestId)
+                .orElseThrow(() -> new ServiceRequestNotFound(serviceRequestId));
+    }
+
+    // QuoteRequest
+    public static QuoteRequest quoteRequestExists(QuoteRequestRepository repository, long quoteRequestId)
+            throws QuoteRequestNotFound {
+        return repository.findById(quoteRequestId).orElseThrow(() -> new QuoteRequestNotFound(quoteRequestId));
+    }
+
+    // Quote
+    public static Quote quoteExists(QuoteRepository repository, long quoteId) throws QuoteNotFound {
+        return repository.findById(quoteId).orElseThrow(() -> new QuoteNotFound(quoteId));
+    }
+
+    // Appointment
+    public static Appointment appointmentExists(AppointmentRepositoryTest repository, long appointmentId)
+            throws AppointmentNotFound {
+        return repository.findById(appointmentId).orElseThrow(() -> new AppointmentNotFound(appointmentId));
+    }
+
+    // SupplierServiceJoin
+    public static SupplierServiceJoin supplierServiceJoinExists(SupplierServiceJoinRepository repository,
+            SupplierServiceKey supplierServiceKey) throws ServiceNotAssignedException {
+        return repository.findById(supplierServiceKey)
+                .orElseThrow(() -> new ServiceNotAssignedException(supplierServiceKey.getServiceId()));
+    }
+
+    public static void verifySupplierServiceJoinExists(SupplierServiceJoinRepository repository,
+            SupplierServiceKey supplierServiceKey) throws ServiceNotAssignedException {
+        if (!repository.existsById(supplierServiceKey)) {
+            throw new ServiceNotAssignedException(supplierServiceKey.getServiceId());
+        }
+    }
+
+    // Address
+    public static Address addressExists(AddressRepository repository, long addressId) throws AddressNotFound {
+        return repository.findById(addressId).orElseThrow(() -> new AddressNotFound(addressId));
+    }
+
+}
