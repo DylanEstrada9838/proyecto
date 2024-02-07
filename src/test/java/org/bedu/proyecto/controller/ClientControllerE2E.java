718280ec01334b1b25b90f8bde9bb213e3260e48
dylanestrada <dylaneb9861@gmail.com>
2024-02-06 21:55:32 -0600

    CD test

--- src/test/java/org/bedu/proyecto/controller/ClientControllerE2E.java
+++ src/test/java/org/bedu/proyecto/controller/ClientControllerE2E.java
@@ -1,33 +1,33 @@
-package org.bedu.proyecto.controller;
+// package org.bedu.proyecto.controller;
 
-import org.bedu.proyecto.repository.ClientRepository;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.web.servlet.MockMvc;
+// import org.bedu.proyecto.repository.ClientRepository;
+// import org.junit.jupiter.api.BeforeEach;
+// import org.junit.jupiter.api.extension.ExtendWith;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+// import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+// import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+// import org.springframework.boot.test.context.SpringBootTest;
+// import org.springframework.test.context.junit.jupiter.SpringExtension;
+// import org.springframework.test.web.servlet.MockMvc;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+// import com.fasterxml.jackson.databind.ObjectMapper;
 
-@AutoConfigureTestDatabase(replace = Replace.NONE)
-@AutoConfigureMockMvc
-@ExtendWith(SpringExtension.class)
-@SpringBootTest
-class ClientControllerE2E {
-    @Autowired
-  private MockMvc mockMvc;
+// @AutoConfigureTestDatabase(replace = Replace.NONE)
+// @AutoConfigureMockMvc
+// @ExtendWith(SpringExtension.class)
+// @SpringBootTest
+// class ClientControllerE2E {
+//     @Autowired
+//   private MockMvc mockMvc;
 
-  @Autowired
-  private ClientRepository repository;
+//   @Autowired
+//   private ClientRepository repository;
 
-  private ObjectMapper mapper = new ObjectMapper();
+//   private ObjectMapper mapper = new ObjectMapper();
 
-  @BeforeEach
-  public void setup() {
-    repository.deleteAll();
-  }
-}
+//   @BeforeEach
+//   public void setup() {
+//     repository.deleteAll();
+//   }
+// }
