b8b250681e875cb7be2bcb56e412c53400c95d49
dylanestrada <dylaneb9861@gmail.com>
2024-02-11 23:10:22 -0600

    Added cors configuration

--- src/main/java/org/bedu/proyecto/config/SecurityConfiguration.java
+++ src/main/java/org/bedu/proyecto/config/SecurityConfiguration.java
@@ -1,5 +1,8 @@
 package org.bedu.proyecto.config;
 
+
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -9,6 +12,9 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import lombok.RequiredArgsConstructor;
 
@@ -21,9 +27,22 @@ public class SecurityConfiguration {
     private final AuthenticationProvider authenticationProvider;
 
     @Bean
+CorsConfigurationSource corsConfigurationSource() {
+	CorsConfiguration configuration = new CorsConfiguration();
+	configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+	configuration.setAllowedMethods(Arrays.asList("*"));
+    configuration.setAllowedHeaders(Arrays.asList("*"));
+    configuration.setExposedHeaders(Arrays.asList("*"));
+    configuration.setAllowedOriginPatterns(Arrays.asList("*"));
+    configuration.setAllowCredentials(true);
+	UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	source.registerCorsConfiguration("/**", configuration);
+	return source;
+}
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
        http.csrf(csrf -> csrf.disable());
-       http.cors(cors->cors.disable());
+       http.cors(cors->cors.configurationSource(corsConfigurationSource()));
        http.authorizeHttpRequests(request->{
         request.requestMatchers("/users/sign-in","/users/sign-up").permitAll();
         request.requestMatchers(HttpMethod.GET,"/users").hasRole("ADMIN");
--- src/test/java/org/bedu/proyecto/service/ServiceRequestServiceTest.java
+++ src/test/java/org/bedu/proyecto/service/ServiceRequestServiceTest.java
@@ -1,5 +1,39 @@
 package org.bedu.proyecto.service;
 
-public class ServiceRequestServiceTest {
-    
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import org.bedu.proyecto.mapper.ServiceRequestMapper;
+import org.bedu.proyecto.repository.AddressRepository;
+import org.bedu.proyecto.repository.ClientRepository;
+import org.bedu.proyecto.repository.ServiceRepository;
+import org.bedu.proyecto.repository.ServiceRequestRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+@ExtendWith(MockitoExtension.class)
+@SpringBootTest
+class ServiceRequestServiceTest {
+    @MockBean
+    private ServiceRequestRepository repository;
+    @MockBean
+    private ServiceRequestMapper mapper;
+    @MockBean
+    private ClientRepository clientRepository;
+    @MockBean
+    private ServiceRepository serviceRepository;
+    @MockBean
+    private AddressRepository addressRepository;
+    @Autowired
+    ServiceRequestService service;
+
+    @Test
+    @DisplayName("Service should be injected")
+    void smokeTest() {
+        assertNotNull(service);
+    }
+
 }
